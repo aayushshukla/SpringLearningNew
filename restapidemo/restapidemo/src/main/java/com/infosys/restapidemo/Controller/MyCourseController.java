3c389d8372e5af492a4c0d114f3bd98b982d8223
Aayush Shukla <aayush.shukla1012@gmail.com>
2024-07-10 16:02:38 +0530

    Add files via upload

--- restapidemo/restapidemo/src/main/java/com/infosys/restapidemo/Controller/MyCourseController.java
+++ restapidemo/restapidemo/src/main/java/com/infosys/restapidemo/Controller/MyCourseController.java
@@ -1,15 +1,23 @@
 package com.infosys.restapidemo.Controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController // (@Controller + @ResponseBody)
+@RequestMapping("courseservice")
 public class MyCourseController {
 	@Autowired
 	CourseRepository courseRepository;
@@ -92,5 +101,57 @@ public class MyCourseController {
 	{
 		return courseRepository.findById(courseId).orElseThrow(()-> new CourseException(courseId));
 	}
+	
+	@GetMapping("/allcourses")
+	public List<Course> getAllCourses()
+	{
+		// ;
+		 //List<Course> courseList =   courseRepository.findAll();
+		List<Course> courseList = new ArrayList<Course>();
+		courseRepository.findAll().forEach(course -> courseList.add(course) );
+	   
+	    return courseList;
+	}
+	
+	@DeleteMapping("/removecourse")
+	public ResponseEntity<CourseResponse>  deleteCourse(RequestEntity<Course> requestEntity)
 	 
+	{
+	   HttpHeaders headers= requestEntity.getHeaders();
+	   headers.forEach((key,value)->{
+		   System.err.println("\t"+key+"=->"+value+"\n");
+	   });
+		Course  mycourse = requestEntity.getBody();
+		log.info(" "+mycourse.getCourseId());
+		courseRepository.deleteById(mycourse.getCourseId());
+		CourseResponse response = new CourseResponse();
+		response.setStatusCode(200);
+		response.setStatusMsg("Course removed successfully");
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+		
+	}
+	
+	@PatchMapping("updatecourse")
+	public ResponseEntity<CourseResponse>  updateCourse(@RequestBody Course newcourse)
+	{
+		CourseResponse response = new CourseResponse();
+		Optional<Course>  oldcourse =  courseRepository.findById(newcourse.getCourseId());
+		if(oldcourse.isPresent())
+		{
+			oldcourse.get().setCourseName(newcourse.getCourseName());
+			oldcourse.get().setDuration(newcourse.getDuration());
+			courseRepository.save(oldcourse.get());
+		}
+		else
+		{
+			response.setStatusCode(400);
+			response.setStatusMsg("Course Not Found");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+			
+		}
+		
+		response.setStatusCode(200);
+		response.setStatusMsg("New Course Information Is Updated");
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
 }
