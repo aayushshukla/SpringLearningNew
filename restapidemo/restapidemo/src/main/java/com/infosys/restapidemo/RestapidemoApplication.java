5897851451d5b74488f5f09ecaefddda9f5ac016
Aayush Shukla <aayush.shukla1012@gmail.com>
2024-07-13 14:10:07 +0530

    Add files via upload

--- restapidemo/restapidemo/src/main/java/com/infosys/restapidemo/RestapidemoApplication.java
+++ restapidemo/restapidemo/src/main/java/com/infosys/restapidemo/RestapidemoApplication.java
@@ -2,8 +2,10 @@ package com.infosys.restapidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RestapidemoApplication {
 
 	public static void main(String[] args) {
